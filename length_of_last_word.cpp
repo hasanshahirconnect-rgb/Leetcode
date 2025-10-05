933e364430bfe309d4d7bb7d40774cf6e9e8d11c
Hasan Shahir <hasanshahirconnect@gmail.com>
2025-10-05 12:56:03 +0500

    Add Length of last word solution

---
+++ length_of_last_word.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int lengthOfLastWord(char* s) {
+    int len = strlen(s);
+    int count = 0;
+    int i = len - 1;
+    
+    while (i >= 0 && s[i] == ' ') i--;
+    while (i >= 0 && s[i] != ' ') {
+        count++;
+        i--;
+    }
+    return count;
+}
+
+int main() {
+    printf("Last word length: %d\n", lengthOfLastWord("Hello World"));
+    return 0;
+}
